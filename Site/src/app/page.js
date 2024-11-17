2a95d0a1231efc2a9753e5aff1e0f33a01b8737e
Caio Sampaio <39320152+CaioSampaio@users.noreply.github.com>
2024-11-17 15:53:50 -0300

    Arquivos do Site

---
+++ Site/src/app/page.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <section>
+
+        </section>
+        <section>
+          <div>
+
+          </div>
+          <div>
+            
+          </div>
+        </section>
+      </main>
+      <footer className={styles.footer}>
+        <p>Não reembolsamos os valores do ingresso, não insista.</p>
+      </footer>
+    </div>
+  );
+}
---
+++ Site/src/components/sala/index.jsx
@@ -0,0 +1,14 @@
+'client url'
+
+import { collectSegments } from "next/dist/build/segment-config/app/app-segments";
+import styles from "./quadro.module.css";
+
+const cadeiras = map.src('../public/filme.json');
+
+export default function Quadro({url, titulo, autor, ano}){
+    return (
+        <div>
+            
+        </div>
+    );
+}
